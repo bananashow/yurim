5c07b3a567f6fd87b53c38de5afb634353c11c93
bananaqick <bananaqick@gmail.com>
2024-02-22 11:56:47 +0900

    test : 임시 이미지 데이터 생성

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,1 +1,85 @@
 export const NAV_MENU = ['홈', '신축 인테리어', '리모델링', '부분디자인', '현장이야기', 'CONTACT'];
+
+export const TEST_IMAGE_DATA = [
+  {
+    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+    title: 'Bed',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    title: 'Books',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    title: 'Sink',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    title: 'Kitchen',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    title: 'Blinds',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    title: 'Chairs',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    title: 'Laptop',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    title: 'Doors',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: 'Storage',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    title: 'Candle',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    title: 'Coffee table',
+  },
+];
+
+export const TEST_SAME_IMAGE_DATA = [
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: '110평 대저택! 자연과 공존하는 "트인 집"',
+    review:
+      '회사분위기가 전체적으로 따뜻하고 친절했습니다. 소통을 잘해주셔서 원하는데로 디자인을 진행할 수 있었고 문의사항이나 수정사항 등의 응대를 바로바로 친절하게 해주셨습니다. 시공기간 동안 거의 매일 작업사진들을 보내 주셔서 직접 가보지 못해도 진행사항을 알 수있어 좋았습니다. 시공 퀄리티도 좋았고 전반적으로 매우 만족스러운 인테리어였습니다.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62?w=248&fit=crop&auto=format&dpr=2 2x',
+    title: '우리집 짱짱맨',
+    review:
+      '회사분위기가 전체적으로 따뜻하고 친절했습니다. 소통을 잘해주셔서 원하는데로 디자인을 진행할 수 있었고 문의사항이나 수정사항 등의 응대를 바로바로 친절하게 해주셨습니다. 시공기간 동안 거의 매일 작업사진들을 보내 주셔서 직접 가보지 못해도 진행사항을 알 수있어 좋았습니다. 시공 퀄리티도 좋았고 전반적으로 매우 만족스러운 인테리어였습니다.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: '너네집도 짱짱맨',
+    review:
+      '회사분위기가 전체적으로 따뜻하고 친절했습니다. 소통을 잘해주셔서 원하는데로 디자인을 진행할 수 있었고 문의사항이나 수정사항 등의 응대를 바로바로 친절하게 해주셨습니다. 시공기간 동안 거의 매일 작업사진들을 보내 주셔서 직접 가보지 못해도 진행사항을 알 수있어 좋았습니다. 시공 퀄리티도 좋았고 전반적으로 매우 만족스러운 인테리어였습니다.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62?w=248&fit=crop&auto=format&dpr=2 2x',
+    title: '인테리어 하실?',
+    review:
+      '회사분위기가 전체적으로 따뜻하고 친절했습니다. 소통을 잘해주셔서 원하는데로 디자인을 진행할 수 있었고 문의사항이나 수정사항 등의 응대를 바로바로 친절하게 해주셨습니다. 시공기간 동안 거의 매일 작업사진들을 보내 주셔서 직접 가보지 못해도 진행사항을 알 수있어 좋았습니다. 시공 퀄리티도 좋았고 전반적으로 매우 만족스러운 인테리어였습니다.',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: '우리집에 왜 왔니',
+    review:
+      '회사분위기가 전체적으로 따뜻하고 친절했습니다. 소통을 잘해주셔서 원하는데로 디자인을 진행할 수 있었고 문의사항이나 수정사항 등의 응대를 바로바로 친절하게 해주셨습니다. 시공기간 동안 거의 매일 작업사진들을 보내 주셔서 직접 가보지 못해도 진행사항을 알 수있어 좋았습니다. 시공 퀄리티도 좋았고 전반적으로 매우 만족스러운 인테리어였습니다.',
+  },
+];

0c85e7ee0e905dbbfbe0fed5cfa5efe4951fbcfd
bananaqick <bananaqick@gmail.com>
2024-03-25 15:10:51 +0900

    feat : 스크롤 상단으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Layout } from './components/common/Layout';
 import { Navbar } from './components/common/NavBar';
 import { Router } from './routes/Router';
 import { Footer } from './components/footer/Footer';
+import { ScrollToTop } from './components/common/ScrollToTop';
 
 export const App = () => {
   return (
     <>
       <Layout>
         <>
+          <ScrollToTop />
           <Navbar />
           <Router />
           <Dial />
